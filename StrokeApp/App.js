ecceec31d9b619d868dd07e57072818485dec189
scottmaclennan <scottmaclennan30@gmail.com>
2024-03-31 22:31:01 +0100

    changes made

--- StrokeApp/App.js
+++ StrokeApp/App.js
@@ -1,32 +1,65 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Medical"
+        onPress={() => navigation.navigate('Medical')}
+      />
+      <Button
+        title="Go to Exercise"
+        onPress={() => navigation.navigate('Exercise')}
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function ExerciseTracker({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Medical... again"
+        onPress={() => navigation.push('Medical')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go Medical Tracker" onPress={() => navigation.navigate('Exercise')} />
     </View>
   );
 }
 
+function MedicalTracker({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Exercise... again"
+        onPress={() => navigation.push('Exercise')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go Exercise Tracker" onPress={() => navigation.navigate('Exercise')} />
+
+    </View>
+  );
+}
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{title: 'Overview'}}
+         />
+        <Stack.Screen name="Medical" component={MedicalTracker} />
+        <Stack.Screen name="Exercise" component={ExerciseTracker} />
       </Stack.Navigator>
     </NavigationContainer>
   );
