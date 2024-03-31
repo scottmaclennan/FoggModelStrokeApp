7f0e17478c44dff0680d78697c5296ec57b00230
scottmaclennan <scottmaclennan30@gmail.com>
2024-03-31 21:07:39 +0100

    reset main

--- StrokeApp/App.js
+++ StrokeApp/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MedicalTracker from './screens/MedicalTracker';
-import ExerciseTracker from './screens/ExerciseTracker'; // Import other screens here
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Stack = createNativeStackNavigator();
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={MedicalTracker} />
-        <Stack.Screen name="Other" component={ExerciseTracker} /> // Add other screens here
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
